8218aed9ee0279595d6877bc54a8f8fb8e2cc38a
Avia Yehonadav <31587014+aviaWIS@users.noreply.github.com>
2018-09-12 13:26:42 +0300

    Update ML by Andrew Ng Ex. 1 in Py by AY.py

--- ML by Andrew Ng Ex. 1 in Py by AY.py
+++ ML by Andrew Ng Ex. 1 in Py by AY.py
@@ -1,23 +1,13 @@
-
-# coding: utf-8
-
-# In[338]:
-
-
-# Machine Learning Course by Andrew Ng - Programming Ex. 1 - A Python Solution by Avia Yehonadav
-
 # Import Data
 import pandas as pd
 data1 = pd.read_csv('machine-learning-ex1/ex1/ex1data1.txt', sep="," )
 data1.columns = ['Population of City in 10,000s', 'Profit in $10,000s']
 
-# Part 1 - Plot Data
-import seaborn as sns
-plt1 = sns.scatterplot(x=pdData1['Population of City in 10,000s'], y=pdData1['Profit in $10,000s'], marker="x", color="r")
-plt1.set(ylim=(-5, None),xlim=(4, None))
-plt1.set_title("Figure 1: Scatter plot of training data")
-
 #Implement Gradient Descent for Linear Regerssion (y = mx + b)
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as pyplot
+
 def gradient_descent(x,y):
     m_temp = 0
     b_temp = 0
@@ -28,7 +18,7 @@ def gradient_descent(x,y):
     for i in range(itr):
         y_pre = m_temp*x + b_temp
         fcost = lambda y,y_pre: (1/n)*sum((y-y_pre)**2)      #something here seems a bit off?
-        cost = fcost(y,y_pre)                                 #Make it into one line aomehow?
+        cost = fcost(y,y_pre)                                 #Make it into one line somehow?
         dm = -(2/n)*sum(x*(y-y_pre))
         db = -(2/n)*sum(y-y_pre)
         m_temp = m_temp - alpha*dm
@@ -42,15 +32,11 @@ def gradient_descent(x,y):
     for px in prex:
         prey.append(FoX(px))
         
-    plt1 = sns.scatterplot(x=pdData1['Population of City in 10,000s'], y=pdData1['Profit in $10,000s'], marker="x", color="r")
+    plt1 = sns.scatterplot(x=data1['Population of City in 10,000s'], y=data1['Profit in $10,000s'], marker="x", color="r")
     plt1.set(ylim=(-5, None),xlim=(4, None))
     plt1.set_title("Figure 1: Scatter plot of training data")
-    pyplot.plot(prex,prey)
-    
-    
+    pyplot.plot(prex,prey)   
 
 xs = np.array(data1[data1.columns[0]])
 ys = np.array(data1[data1.columns[1]])               
 gradient_descent(xs,ys)
-
-
